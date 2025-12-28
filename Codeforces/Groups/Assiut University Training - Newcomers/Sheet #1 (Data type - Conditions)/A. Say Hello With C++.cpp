9fe7450b374084695afa39a093058a474442231b
MdHasanShikder <70508306+MdHasanShikder@users.noreply.github.com>
2025-12-28 21:54:37 +0600

    Update A. Say Hello With C++.cpp
    Added comments for learning purposes and included necessary headers for string usage.

--- Codeforces/Groups/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/A. Say Hello With C++.cpp
+++ Codeforces/Groups/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/A. Say Hello With C++.cpp
@@ -1,1 +1,17 @@
+/*
+Author: hasan2
+Learning (for myself):
+1. to use string i must add <string> header file.
+2. to define a string i have to use 'string' data type.
+2. cin considers space as termination. so if i want to input a line i must use getline(cin, stringName); function
+*/
 
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    getline(cin, s);
+    cout << "Hello, " << s << endl;
+    return 0;
+}
